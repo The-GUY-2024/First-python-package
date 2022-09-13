c6279e1641013983032279849e4b3d8f1b9b7827
Certified Humane <castrolwilliam@gmail.com>
2022-09-13 16:24:53 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='Mypack',
+    version='0.1',
+    description='This is my first module',
+    url='#',
+    author='Louis William',
+    author_email='castrolwilliam@gmail.com',
+    license='MIT',
+    packages=['Mypack'],
+    zip_safe=False
+
+)
---
+++ test.py
@@ -0,0 +1,3 @@
+from mypackage import power, average, SayHello
+
+SayHello("Hello")
